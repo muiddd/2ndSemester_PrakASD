212baa2fb12222dc7cf0404280ddcb3bf8db36ac
muiddd <muidabdulmuid99@gmail.com>
2025-02-26 12:18:39 +0700

    P3_Percobaan3_PertanyaanNo4

--- P3/MataKuliahDemo01.java
+++ P3/MataKuliahDemo01.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
+
 public class MataKuliahDemo01 {
     public static void main(String[] args) {
-        MataKuliah01[] arrayOfMataKuliah = new MataKuliah01[3];
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
+        System.out.print("Masukkan jumlah Matakuliah : ");
+        int jml = sc.nextInt();
+        MataKuliah01[] arrayOfMataKuliah = new MataKuliah01[jml];
+
+        for (int i = 0; i < arrayOfMataKuliah.length; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i+1));
             arrayOfMataKuliah[i] = new MataKuliah01();
             arrayOfMataKuliah[i].tambahData();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayOfMataKuliah.length; i++) {
             System.out.println("Data Matakuliah ke-" + (i+1));
             arrayOfMataKuliah[i].cetakInfo();
         }
+
+        sc.close();
     }
 }
