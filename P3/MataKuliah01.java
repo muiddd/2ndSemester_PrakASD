b864edd024ac27dde88c31e977b33aefb91350bf
muiddd <muidabdulmuid99@gmail.com>
2025-02-26 11:17:58 +0700

    P3_Percobaan3

---
+++ P3/MataKuliah01.java
@@ -0,0 +1,17 @@
+public class MataKuliah01 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public MataKuliah01(){
+
+    }
+
+    public MataKuliah01(String kode, String nama, int sks, int jumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
