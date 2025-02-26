65d9d856bd34e5c251eed65542c25bf604256484
muiddd <muidabdulmuid99@gmail.com>
2025-02-26 10:31:30 +0700

    P3_Percobaan2_PertanyaanNo1

--- P3/MahasiswaDemo01.java
+++ P3/MahasiswaDemo01.java
@@ -23,13 +23,9 @@ public class MahasiswaDemo01 {
             System.out.println("----------------------------------");
         }
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Data Mahasiswa ke-" + (i+1));
-            System.out.println("NIM   : "+ arrayOfMahasiswa[i].nim);
-            System.out.println("Nama  : "+ arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas : "+ arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK   : "+ arrayOfMahasiswa[i].ipk);
-            System.out.println("----------------------------------");
+        for (int i = 0; i < arrayOfMahasiswa.length; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+            arrayOfMahasiswa[i].cetakInfo();
         }
 
         sc.close();
