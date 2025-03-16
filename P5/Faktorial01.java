26370c4810e3177c03f0c39d1fc3bb3f8e0beb92
muiddd <muidabdulmuid99@gmail.com>
2025-03-16 10:40:23 +0700

    P5_Percobaan1_PertanyaanNo2

--- P5/Faktorial01.java
+++ P5/Faktorial01.java
@@ -1,18 +1,21 @@
 package P5;
 
 public class Faktorial01 {
-    int faktoriaBF(int n){
+    int faktoriaBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <=n ; i++) {
-            fakto = fakto *i;
+        int i = 1;
+        while (i <= n) {
+            fakto *= i;
+            i++;
         }
         return fakto;
     }
-    int faktorialDC(int n){
-        if(n==1){
+
+    int faktorialDC(int n) {
+        if (n == 1) {
             return 1;
-        }else {
-            int fakto = n * faktorialDC(n-1);
+        } else {
+            int fakto = n * faktorialDC(n - 1);
             return fakto;
         }
     }
