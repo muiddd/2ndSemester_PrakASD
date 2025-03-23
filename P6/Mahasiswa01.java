8851d36b6616d502fc10221ac05b8def72ccc210
muiddd <muidabdulmuid99@gmail.com>
2025-03-23 23:39:03 +0700

    P6_Percobaan2b

--- P6/Mahasiswa01.java
+++ P6/Mahasiswa01.java
@@ -19,8 +19,7 @@ public class Mahasiswa01 {
     void tampilInformasi (){
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
-        System.out.println("IPK: " + ipk);
         System.out.println("Kelas: " + kelas);
-        
+        System.out.println("IPK: " + ipk);
     }
 }
--- P6/MahasiswaDemo.java
+++ P6/MahasiswaDemo.java
@@ -14,25 +14,27 @@ public class MahasiswaDemo {
         
         for (int i = 0; i < jml; i++) {
             System.out.println("\nMasukkan data mahasiswa ke-"+ (i+1));
-            System.out.print("Nama: ");
-            String nama = sc.nextLine();
             System.out.print("NIM: ");
             String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
             System.out.print("IPK: ");
             double ipk = sc.nextDouble();
             sc.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
-
+            
             Mahasiswa01 m = new Mahasiswa01(nim,nama,kelas,ipk);
             list.tambah(m);
         }
         
-        System.out.println("\nData mahasiswa sebelum sorting: ");
-        list.tampil();
         
         System.out.println("\nData Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
         list.bubbleSort();
+        list.tampil();
+
+        System.out.println("\nData yang sudah terurut menggunakan SELECTION SORT (ASC): ");
+        list.selectionSort();
         list.tampil();
 
         sc.close();
