d425f74519e7b4979e61803aba8c4ea75b2ec76e
muiddd <muidabdulmuid99@gmail.com>
2025-03-26 08:33:25 +0700

    P7_Percobaan1

---
+++ P7/MahasiswaDemo01.java
@@ -0,0 +1,43 @@
+package P7;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo01 {
+    public static void main(String[] args) {        
+        Scanner sc = new Scanner(System.in);
+        MahasiswaBerprestasi01 list = new MahasiswaBerprestasi01();
+        int jumMhs = 5;
+    
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Masukkan data mahasiswa ke-"+ (i+1));
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK: ");
+            String ip = sc.nextLine();
+            double ipk = Double.parseDouble(ip);
+            System.out.println("-----------------------------");
+            list.tambah(new Mahasiswa01(nim,nama,kelas,ipk));
+        }
+        
+        list.tampil();
+
+        System.out.println("---------------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("---------------------------------------------------");
+        System.out.println("Masukkan ipk mahasiswa yang ingin dicari:");
+        System.out.print("IPK: ");
+        double cari = sc.nextDouble();
+
+        System.out.println("menggunakan sequential searching");
+        double posisi = list.sequentialSearching(cari);
+        int pss= (int)posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
+
+        sc.close();
+    }
+}
