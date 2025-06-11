9aa77df49e1aea6134304eb31ac05244debfcb05
muiddd <muidabdulmuid99@gmail.com>
2025-06-11 13:50:47 +0700

    P15_TugasNo4

--- P15/BinaryTreeArray01.java
+++ P15/BinaryTreeArray01.java
@@ -1,14 +1,15 @@
 package P15;
 
 public class BinaryTreeArray01 {
-    Mahasiswa01 [] dataMahasiswa;
+    Mahasiswa01[] dataMahasiswa;
     int idxLast;
 
     public BinaryTreeArray01() {
         this.dataMahasiswa = new Mahasiswa01[10];
+        idxLast = -1;
     }
 
-    void populateData01(Mahasiswa01 dataMhs [], int idxLast) {
+    void populateData01(Mahasiswa01 dataMhs[], int idxLast) {
         this.dataMahasiswa = dataMhs;
         this.idxLast = idxLast;
     }
@@ -16,10 +17,32 @@ public class BinaryTreeArray01 {
     void traverseInOrder01(int idxStart) {
         if (idxStart <= idxLast) {
             if (dataMahasiswa[idxStart] != null) {
-                traverseInOrder01((2*idxStart+1));
+                traverseInOrder01((2 * idxStart + 1));
                 dataMahasiswa[idxStart].tampilInformasi();
-                traverseInOrder01((2*idxStart+2));
+                traverseInOrder01((2 * idxStart + 2));
             }
         }
+    }
+
+    public void add01(Mahasiswa01 data) {
+        if (idxLast == dataMahasiswa.length - 1) {
+            System.out.println("Tree sudah penuh!");
+            return;
+        }
+        idxLast++;
+        dataMahasiswa[idxLast] = data;
+    }
+
+    public void traversePreOrder01() {
+        traversePreOrder01(0);
+    }
+
+    void traversePreOrder01(int idx) {
+        if (idx > idxLast || dataMahasiswa[idx] == null) {
+            return;
+        }
+        dataMahasiswa[idx].tampilInformasi();
+        traversePreOrder01(2 * idx + 1);
+        traversePreOrder01(2 * idx + 2);
     }
 }
